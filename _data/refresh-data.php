83eecc3024d903956d5d04cc2c4a0203c1c37417
Jim Nielsen <jimniels@gmail.com>
2014-02-26 17:42:54 -0500

    Fix for refreshing data for the blog

--- _data/refresh-data.php
+++ _data/refresh-data.php
@@ -1,5 +1,15 @@
 <?php
-
+/* 
+ * Dribbble
+ *
+ */
+// Get response
+function getDribbbleJSON() {
+    $username = 'jimniels';
+    $url = 'http://dribbble.com/'.$username.'/shots.json';
+    $json = file_get_contents($url);
+    return $json;
+}
 // Remove escaped slashes that PHP does using json_encode
 function stripslashes_deep($value) {
     $value = is_array($value) ?
@@ -7,20 +17,6 @@ function stripslashes_deep($value) {
                 stripslashes($value);
     return $value;
 }
-
-
-
-/* 
- * Dribbble
- *
- */
-function getDribbbleJSON() {
-    $username = 'jimniels';
-    $url = 'http://dribbble.com/'.$username.'/shots.json';
-    $json = file_get_contents($url);
-    return $json;
-}
-
 
 $dribbble = json_decode(getDribbbleJSON(), true);
 
@@ -49,40 +45,34 @@ if ( $dribbble['shots'][0]['title'] != '' || $dribbble['shots'][0]['title'] != n
  * BLOG
  *
  */
-// function getBlogJSON() {
-    
-//     // get XML feed
-//     $url = 'http://jim-nielsen.com/blog/feed.xml';
+function getBlogJSON() {
+    $url = 'http://jim-nielsen.com/blog/feed.json';
+    $json= file_get_contents($url);
+    return $json;
+}
 
-//     // convert XML to JSON
-//     $fileContents= file_get_contents($url);
-//     $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
-//     $fileContents = trim(str_replace('"', "'", $fileContents));
-//     $simpleXml = simplexml_load_string($fileContents);
-//     $json = json_encode($simpleXml);
+$response = getBlogJSON();
+$response_array = json_decode($response, true);
 
-//     return $json;
-// }
+// Verify and write data
+if ($response_array['posts'][0]['title'] != '' || 
+    $response_array['posts'][0]['title'] != null) {
 
-// $blog = json_decode(getBlogJSON(), true);
+    echo "Verifying Blog ... \n";
 
-// // Verify and write data
-// if ( $blog['channel']['item'][0]['title'] != '' || $blog['channel']['item'][0]['title'] != null) {
-//     echo "Verifying Blog ... \n";
+    $file = 'blog.yml';
 
-//     $file = 'blog.yml';
-
-//     if ( file_exists($file) ) {
-//         $f = file_put_contents($file, stripslashes_deep( json_encode($blog) ) );
-//         if($f) {
-//             echo "SUCCESS! New blog data written to cache!";
-//             //echo json_encode($blog);
-//         } else {
-//             echo "WARNING: Failed writing blog cache file.";
-//         }
-//     } else {
-//         echo 'WARNING: Could not write to blog cache file. File does not exist?';
-//     }
-// } else {
-//     echo 'WARNING: Blog data could not be verified. Something wrong with XML Feed?';   
-// }
+    if ( file_exists($file) ) {
+        $f = file_put_contents($file, $response);
+        if($f) {
+            echo "SUCCESS! New blog data written to cache!";
+            //echo json_encode($blog);
+        } else {
+            echo "WARNING: Failed writing blog cache file.";
+        }
+    } else {
+        echo 'WARNING: Could not write to blog cache file. File does not exist?';
+    }
+} else {
+    echo 'WARNING: Blog data could not be verified. Something wrong with XML Feed?';   
+}
