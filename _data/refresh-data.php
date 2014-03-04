793ceb38173abe5ec3d92e6ecc390489b662989d
Jim Nielsen <jimniels@gmail.com>
2014-03-04 12:47:43 -0500

    Refresh data now checks to see if current dribbble data matches the API response

--- _data/refresh-data.php
+++ _data/refresh-data.php
@@ -21,24 +21,29 @@ function stripslashes_deep($value) {
 $dribbble = json_decode(getDribbbleJSON(), true);
 
 // Verify and write Dribbble
-if ( $dribbble['shots'][0]['title'] != '' || $dribbble['shots'][0]['title'] != null) {
+if ( $dribbble['shots'][0]['title'] != '' || $dribbble['shots'][0]['title'] != null ) {
     echo "Verifying Dribbble ... \n";
 
+    // see if data is up to date
     $file = 'dribbble.yml';
-
-    if ( file_exists($file) ) {
-        $f = file_put_contents($file, stripslashes_deep( json_encode($dribbble) ) );
-        if($f) {
-            echo "SUCCESS! New dribbble data written to cache!";
-            //echo json_encode($dribbble);
-        } else {
-            echo "WARNING: Failed writing dribbble cache file.";
-        }
+    $oldDribbble = json_decode(file_get_contents($file), true);
+    
+    if( $dribbble['shots'][0]['title'] == $oldDribbble['shots'][0]['title'] ){
+        echo "NOTE: Dribbble data is up to date. \n";
     } else {
-        echo 'WARNING: Could not write to dribbble cache file. File does not exist?';
+        if ( file_exists($file) ) {
+            $f = file_put_contents($file, stripslashes_deep( json_encode($dribbble) ) );
+            if($f) {
+                echo "SUCCESS! New dribbble data written to cache! \n";
+            } else {
+                echo "WARNING: Failed writing dribbble cache file. \n";
+            }
+        } else {
+            echo "WARNING: File does not exist? \n";
+        }
     }
 } else {
-    echo 'WARNING: Dribbble data could not be verified. Something wrong with Dribbble API?';   
+    echo "WARNING: Dribbble data could not be verified. Something wrong with Dribbble API response. \n";   
 }
 
 /* 
@@ -65,14 +70,14 @@ if ($response_array['posts'][0]['title'] != '' ||
     if ( file_exists($file) ) {
         $f = file_put_contents($file, $response);
         if($f) {
-            echo "SUCCESS! New blog data written to cache!";
+            echo "SUCCESS! New blog data written to cache! \n";
             //echo json_encode($blog);
         } else {
-            echo "WARNING: Failed writing blog cache file.";
+            echo "WARNING: Failed writing blog cache file. \n";
         }
     } else {
-        echo 'WARNING: Could not write to blog cache file. File does not exist?';
+        echo "WARNING: Could not write to blog cache file. File does not exist? \n";
     }
 } else {
-    echo 'WARNING: Blog data could not be verified. Something wrong with XML Feed?';   
+    echo "WARNING: Blog data could not be verified. Something wrong with XML Feed? \n";   
 }
