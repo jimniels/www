8c2095ade9a9537dbd1095980c83c0c6784e1ba7
Jim Nielsen <jimniels@gmail.com>
2014-07-30 09:34:18 -0400

    Ready for the prime time!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    
+
     $('body').addClass('js');
 
     $('.section').each(function(){
         $this = $(this);
         $this.addClass('section--condensed');
-        var size = $this.find('.section__content li').size();
+        var size = $this.find('.section__content > li').size();
         $('<p class="section__trigger"><a href="#">View All (' + size + ')</a></p>').appendTo($this);
     });
 
@@ -14,16 +14,11 @@ $(document).ready(function(){
         e.preventDefault();
         var $this = $(this),
             $parent = $this.parents('.section');
-        
+
         if ( $parent.hasClass('section--condensed') ) {
             $parent.removeClass('section--condensed');
-            //$this.text('View Less');
             $this.remove()
-        } 
-        // else {
-        //     $parent.addClass('section--condensed');
-        //     $this.text('View More');
-        // }
+        }
     });
 
 });
