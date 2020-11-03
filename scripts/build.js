5eace9b507ea2b85b8e181d93a180cad002450b7
Jim Nielsen <jimniels@gmail.com>
2020-11-03 16:03:49 -0700

    tweaks

--- scripts/build.js
+++ scripts/build.js
@@ -1,38 +1,261 @@
-const fs = require("fs");
-const path = require("path");
-const fetch = require("node-fetch");
-const Mustache = require("mustache");
-const striptags = require("striptags");
-const YAML = require("yamljs");
-const moment = require("moment");
-const fse = require("fs-extra");
-const slugify = require("./slugify");
+import fs from "fs-extra";
+import Mustache from "mustache";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import fetch from "node-fetch";
+import moment from "moment";
+import YAML from "yamljs";
+import slugifyFn from "slugify";
 
-const SRC = path.resolve(__dirname, "../src");
-const PUBLIC = path.resolve(__dirname, "../public");
-const BUILD = path.resolve(__dirname, "../build");
+const requireJSON = (filepath) =>
+  JSON.parse(fs.readFileSync(filepath).toString());
 
-/**
- * Overview
- * Copy everthing in `./public` into `./build`
- * Then do any file processing in `.src/` you need to and place it in `./build`
- */
-fse.copySync(PUBLIC, BUILD);
+const slugify = (str) => slugifyFn(str, { remove: /['':;,]/g, lower: true });
+const DATE_FORMAT = "MMM D, YYYY";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-let sections = YAML.load(path.resolve(SRC, "data/sections.yml"));
+main();
 
-// Kick it off by getting our data then doing something with it
-try {
-  require("./getData")(async function (data) {
+async function main() {
+  try {
+    const data = await getData();
+
+    fs.copySync(join(__dirname, "../src/www"), join(__dirname, "../build"));
+
     const out = Mustache.render(
-      fs.readFileSync(path.resolve(SRC, "index.html")).toString(),
+      fs
+        .readFileSync(join(__dirname, "../src/www/index.mustache.html"))
+        .toString(),
       data
     );
 
-    fs.writeFileSync(path.resolve(BUILD, "index.html"), out);
+    fs.writeFileSync(join(__dirname, "../build/index.html"), out);
+
     console.log("---> Done!");
-  });
-} catch (e) {
-  console.log(e);
-  console.log("---> Error");
+  } catch (e) {
+    console.log(e);
+    console.log("---> Error");
+  }
+}
+
+async function getData() {
+  return {
+    css: fs.readFileSync(join(__dirname, "../src/www/index.css")),
+    /**
+     * Dribbble
+     * http://developer.dribbble.com/v2/shots/
+     *
+     * In:
+     * [
+     *   {
+     *     description
+     *     title
+     *     html_url
+     *     published_at
+     *     images: {
+     *       hidpi,
+     *       normal,
+     *       teaser
+     *     }
+     *   }
+     * ]
+     *
+     * Out:
+     * [
+     *   { shotUrl, imgUrl }
+     * ]
+     */
+    dribbble: await (async () => {
+      const cacheFile = join(__dirname, "../cache/dribbble.json");
+      let data;
+
+      if (fs.existsSync(cacheFile)) {
+        data = requireJSON(cacheFile);
+      } else {
+        const ids = YAML.load(join(__dirname, "../src/data/dribbble.yml"));
+        data = await Promise.all(
+          ids.map((id) =>
+            fetch(`https://api.dribbble.com/v2/shots/${id}`, {
+              headers: {
+                Authorization:
+                  "Bearer 8379fd23942fcf12e4dfdb3d09293a1c01c19e7a577b35e78d48bb659480a21d",
+                // This header is supposed to return a key name `description_text` where
+                // HTML is stripped, but it's not for some reason. So we'll do it ourself.
+                Accept: "application/vnd.dribbble.v2.text+json",
+              },
+            }).then((res) => res.json())
+          )
+        );
+        fs.outputFileSync(cacheFile, JSON.stringify(data));
+      }
+
+      return data.map(({ html_url, images, title }) => ({
+        href: html_url,
+        src: images.hidpi ? images.hidpi : images.normal,
+        title,
+      }));
+    })(),
+
+    /**
+     * Instagram
+     * https://developers.facebook.com/docs/instagram-basic-display-api/reference/media/
+     * Unfortunately these tokens only appear to last for 60 days. You can refresh them
+     * but that seems like a hassle for our scenario here.
+     * @TODO figure out a better way to refresh tokens
+     * https://developers.facebook.com/docs/instagram-basic-display-api/overview#instagram-user-access-tokens
+     * https://developers.facebook.com/docs/instagram-basic-display-api/guides/long-lived-access-tokens#refresh-a-long-lived-token
+     * [
+     *   {
+     *     media_type,
+     *     media_url,
+     *     permalink
+     *   }
+     * ]
+     */
+    instagram: await (async () => {
+      const file = join(__dirname, "../cache/instagram.json");
+      if (fs.existsSync(file)) {
+        return requireJSON(file);
+      }
+
+      const TOKEN = fs.readFileSync(
+        join(__dirname, "../instagram-flyingjpies.token")
+      );
+      const URL = "https://graph.instagram.com";
+      const USER_ID = "17841406109183572";
+
+      const data = await fetch(
+        `${URL}/${USER_ID}?fields=media&access_token=${TOKEN}`
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          const {
+            media: { data },
+          } = json;
+          // media.paging.next is the URL for the next set of posts
+          return Promise.all(
+            data.map(({ id }) =>
+              fetch(
+                `${URL}/${id}?fields=media_type,media_url,permalink&access_token=${TOKEN}`
+              ).then((res) => res.json())
+            )
+          );
+        })
+        .then((posts) => {
+          return posts
+            .filter((post) => post.media_type !== "VIDEO")
+            .slice(0, 12);
+        });
+      fs.outputFileSync(file, JSON.stringify(data));
+      return data;
+    })(),
+
+    /**
+     * Blog
+     * [
+     *   {
+     *     title,
+     *     url,
+     *     date_published: "2018-09-12"
+     *   }
+     * ]
+     */
+    blogPosts: await (async () => {
+      const data = await fetch("https://blog.jim-nielsen.com/feed.json")
+        .then((res) => res.json())
+        .then((res) =>
+          res.items.map((item) => ({
+            ...item,
+            date_published: moment(item.date_published).format(DATE_FORMAT),
+          }))
+        );
+      return data;
+    })(),
+    blogPostsByTag: await (async () => {
+      const data = await fetch(
+        "https://blog.jim-nielsen.com/archive/index.json"
+      )
+        .then((res) => res.json())
+        .then((posts) => {
+          const postsByTag = posts.reduce((acc, post) => {
+            if (post.tags && post.tags.length) {
+              post.tags.forEach((tag) => {
+                if (acc[tag]) {
+                  acc[tag].push(post);
+                } else {
+                  acc[tag] = [post];
+                }
+              });
+            }
+            return acc;
+          }, {});
+
+          return postsByTag;
+        });
+      return data;
+    })(),
+    /**
+     * Icons
+     * [
+     *   {
+     *     type: ios|macos|watchos
+     *     title
+     *     permalink
+     *     artworkUrl
+     *   }
+     * ]
+     */
+    icons: await (async () => {
+      const handleResponse = (res, type) => {
+        return res.json().then((json) => {
+          return json.items.map((icon) => ({
+            ...icon,
+            type,
+            permalink: icon.url,
+            artworkUrl: icon.attachments[0].url.replace("512", "128"),
+          }));
+        });
+      };
+      const data = await Promise.all([
+        fetch("https://www.iosicongallery.com/feed.json").then((res) =>
+          handleResponse(res, "ios")
+        ),
+        fetch("https://www.macosicongallery.com/feed.json").then((res) =>
+          handleResponse(res, "macos")
+        ),
+        fetch("https://www.watchosicongallery.com/feed.json").then((res) =>
+          handleResponse(res, "watchos")
+        ),
+      ]).then((responses) =>
+        responses.flat().sort((a, b) => {
+          return 0.5 - Math.random();
+        })
+      );
+      return data;
+    })(),
+    publishings: YAML.load(join(__dirname, "../src/data/publishings.yml")).map(
+      (item) => ({
+        ...item,
+        date: moment(item.date).format(DATE_FORMAT),
+        thumbnail:
+          slugify(item.title, { remove: /['':;,]/g, lower: true }) +
+          "." +
+          (item.imageType ? item.imageType : "png"),
+      })
+    ),
+
+    /**
+     * Personal Projects
+     */
+
+    // {
+    //   title: "Employment",
+    //   data: YAML.load(
+    //     path.resolve(__dirname, "../src/data/employment.yml")
+    //   ).map((item) => ({
+    //     ...item,
+    //     thumbnail: item.company.toLowerCase().replace(/ /g, "-") + ".png",
+    //   })),
+    // },
+  };
 }
--- scripts/slugify.js
+++
@@ -1,3 +0,0 @@
-const slugify = require("slugify");
-
-module.exports = str => slugify(str, { remove: /['':;,]/g, lower: true });
