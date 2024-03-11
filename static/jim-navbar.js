241e3b6b9a87a43e29a2bad06b2389f8b86a8700
Jim Nielsen <jimniels@gmail.com>
2024-03-11 10:06:32 -0600

    Update jim-navbar.js

--- static/jim-navbar.js
+++ static/jim-navbar.js
@@ -33,9 +33,6 @@ class jimNavbar extends HTMLElement {
           <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
         </button>
         <div id="dropdown" aria-labelledby="button">
-          <h2>
-            jim-nielsen.com
-          </h2>
           <ul>
             ${sites
               .map(({ title, description }, i) => {
@@ -46,8 +43,11 @@ class jimNavbar extends HTMLElement {
                   <li
                     class="${isActive ? "active" : ""}">
                     <a href="https://${title}jim-nielsen.com">
-                      <span style="font-weight: 700;">${title}</span>
-                      <span style="opacity: .6;">
+                      <span style="display: flex; font-weight: 700;">
+                        <span>${title}</span>
+                        <span style="opacity: .4;">jim-nielsen.com</span>
+                      </span>
+                      <span style="opacity: .7;">
                         ${description}
                       </span>
                     </a>
@@ -88,11 +88,11 @@ class jimNavbar extends HTMLElement {
 
           #dropdown {
             transform: scale(0);
+            padding-bottom: 56px;
             transition: .3s ease transform;
             transform-origin: calc(100% - 20px) calc(100% - 20px);
             position: absolute;
             width: 280px;
-            padding: 0;
             background: #000;
             color: white;
             bottom: -6px;
@@ -114,11 +114,15 @@ class jimNavbar extends HTMLElement {
               list-style: none;
               margin: 0;
               padding: 0;
-              border-bottom: 1px solid #222;
               position: relative;
 
               &:hover {
                 background: #222;
+              }
+
+              &:nth-child(2) {
+                border-bottom: 1px solid #222;
+                border-top: 1px solid #222;
               }
 
               &.active:after {
@@ -160,17 +164,16 @@ class jimNavbar extends HTMLElement {
             margin: 0;
             outline: none;
             border-radius: 50%;
-            border: 2px solid transparent;
+            border: 2px solid #222;
             z-index: 10;
             transition: .6s ease transform;
             transform-origin: 50%;
             cursor: pointer;
             width: 44px;
             height: 44px;
-            box-shadow: 0 0 0 1px #000;
 
             &:focus { outline: none }
-            :focus-visible { box-shadow: 0 0 0 2px blue; }
+            &:focus-visible { box-shadow: 0 0 0 3px blue; }
           }
 
           @media screen and (min-width: 768px) {
@@ -183,15 +186,12 @@ class jimNavbar extends HTMLElement {
               transform-origin: calc(100% - 20px) 20px;
               top: -6px;
               bottom: initial;
+              padding-bottom: 0;
+              padding-top: 56px;
             }
 
             h2 {
               order: initial;
-            }
-
-            ul li {
-              border-bottom: 0;
-              border-top: 1px solid #222;
             }
           }
         </style>
